758292c56b84ee3ca530141802314e7ccbbd7aa1
luxarionadm-design <luxarion.adm@gmail.com>
2026-08-03 21:35:32 +0700

    Add index.js for exporting typed array modules
    Create index.js to export and require multiple array types.

---
+++ src/core/index.js
@@ -0,0 +1,11 @@
+export { Luxarion } from './Luxarion.js';
+export { HybridTypedArray } from './HybridTypedArray.js';
+export { FlexibleTypedArray } from './FlexibleTypedArray.js';
+export { OptimizedTypedArray } from './OptimizedTypedArray.js';
+
+export default {
+  Luxarion: require('./Luxarion.js').Luxarion,
+  HybridTypedArray: require('./HybridTypedArray.js').HybridTypedArray,
+  FlexibleTypedArray: require('./FlexibleTypedArray.js').FlexibleTypedArray,
+  OptimizedTypedArray: require('./OptimizedTypedArray.js').OptimizedTypedArray
+};
