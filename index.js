ffbc58e1d8fee8f1bf0d794f13775a040a17696f
luxarionadm-design <luxarion.adm@gmail.com>
2026-08-03 21:32:44 +0700

    Initialize index.js to export Luxarion Library

---
+++ index.js
@@ -0,0 +1,5 @@
+export * from './src/index.js';
+export { default } from './src/index.js';
+
+console.log('📚 EOCS - Luxarion Library Loaded');
+console.log(`📍 Version: ${process.env.npm_package_version || '1.0.0'}`);
